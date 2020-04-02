fc08ffd44a73efc20cc686a9fdb5b9418fd5408c
V.Sakovets <Sakovvs@tut.by>
2020-04-02 17:17:18 +0300

    done LiveData transformations

--- GuessTheWord-Starter/app/src/main/java/com/example/android/guesstheword/screens/game/GameViewModel.kt
+++ GuessTheWord-Starter/app/src/main/java/com/example/android/guesstheword/screens/game/GameViewModel.kt
@@ -1,11 +1,22 @@
 package com.example.android.guesstheword.screens.game
 
+import android.os.CountDownTimer
+import android.text.format.DateUtils
 import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Transformations
 import androidx.lifecycle.ViewModel
 
 class GameViewModel : ViewModel() {
+    private val timer: CountDownTimer
+
+    companion object {
+        private const val DONE = 0L
+        private const val ONE_SECOND = 1000L
+        private const val COUNTDOWN_TIME = 60000L
+
+    }
 
     private var _word = MutableLiveData<String>()
     val word: LiveData<String>
@@ -19,12 +30,23 @@ class GameViewModel : ViewModel() {
     val eventGameFinish: LiveData<Boolean>
         get() = _eventGameFinish
 
+    private val _currentTime = MutableLiveData<Long>()
+    val currentTime: LiveData<Long>
+        get() = _currentTime
+
+    val currentTimeString = Transformations.map(currentTime) { time ->
+        DateUtils.formatElapsedTime(time)
+    }
+
+    val wordHint = Transformations.map(word) { word ->
+        val randomPosition = (1..word.length).random()
+        "Current word has " + word.length + " letters." +
+                "\nThe letter at position " + randomPosition + " is " +
+                word[randomPosition - 1].toUpperCase()
+    }
+
+
     private lateinit var wordList: MutableList<String>
-
-
-    /**
-     * Resets the list of words and randomizes the order
-     */
     private fun resetList() {
         wordList = mutableListOf("queen", "hospital", "basketball", "cat", "change", "snail", "soup",
                 "calendar", "sad", "desk", "guitar", "home", "railway", "zebra", "jelly", "car", "crow",
@@ -33,18 +55,29 @@ class GameViewModel : ViewModel() {
         wordList.shuffle()
     }
 
-
     init {
         _word.value = ""
         _score.value = 0
         resetList()
         nextWord()
         Log.i("GameViewModel", "GameViewModel created")
+
+        timer = object : CountDownTimer(COUNTDOWN_TIME, ONE_SECOND) {
+            override fun onFinish() {
+                _currentTime.value = DONE
+                onGameFinish()
+            }
+
+            override fun onTick(millisUntilFinished: Long) {
+                _currentTime.value = millisUntilFinished / ONE_SECOND
+            }
+        }
+        timer.start()
     }
 
     private fun nextWord() {
         if (wordList.isEmpty()) {
-            onGameFinish()
+            resetList()
         } else {
             _word.value = wordList.removeAt(0)
         }
@@ -60,9 +93,10 @@ class GameViewModel : ViewModel() {
         nextWord()
     }
 
+    //onCleared()Метод вызывается до того , как ViewModel будет уничтожен.
     override fun onCleared() {
         super.onCleared()
-        Log.i("GameViewModel", "GameViewModel destroyed")
+        timer.cancel()                  //чтобы избежать утечек памяти
 
     }
 
@@ -74,5 +108,4 @@ class GameViewModel : ViewModel() {
         _eventGameFinish.value = false
 
     }
-
 }
